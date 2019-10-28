6a10d5a9443fd4f04d55446b45cbd4fd268aa09d
sinduap <sindu.andita@gmail.com>
2019-10-28 09:46:48 +0700

    Project Completed

--- app.js
+++ app.js
@@ -5,30 +5,39 @@ if(document.readyState == 'loading') {
 }
 
 function ready() {
+    //remove button
     var removeCartButton = document.getElementsByClassName('btn-danger');
     for(var i = 0; i < removeCartButton.length; i++) {
         var button = removeCartButton[i];
         button.addEventListener('click', removeCart)
     }
 
-    //mencegah -value pada quantity input
+    //mencegah negative value pada quantity input
     var inputQuantity = document.getElementsByClassName('cart-quantity-input');
     for(var i = 0; i < inputQuantity.length; i++) {
         var input = inputQuantity[i];
         input.addEventListener('change', quantityChanged);
     }
 
+    //menambah item ke cart
     var addToCartButton = document.getElementsByClassName('shop-item-button');
     for(var i = 0; i < addToCartButton.length; i++) {
         var addToCart = addToCartButton[i];
-        addToCart.addEventListener('click', addToCartClicked); 
+        addToCart.addEventListener('click', addToCartClicked);
     }
+
+    //puchase button
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
 }
 
-function addItemToCart(title, price, image) {
-    var cartRow = createElement('div');
-    cartRow.classList.add('cart-row');
-    
+function purchaseClicked() {
+    var total = document.getElementsByClassName('cart-total-price')[0].innerText;
+    alert('Total ' + total + '\nThank you for the purchase');
+    var cartItems = document.getElementsByClassName('cart-items')[0];
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild);
+    }
+    updateTotalCart()
 }
 
 function addToCartClicked(e) {
@@ -38,7 +47,39 @@ function addToCartClicked(e) {
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
     var image = shopItem.getElementsByClassName('shop-item-image')[0].src;
     addItemToCart(title, price, image);
-    
+}
+
+function addItemToCart(title, price, image) {
+    //buat element div baru
+    var cartRow = document.createElement('div');
+    //tambahkan class untuk styling
+    cartRow.classList.add('cart-row');
+    //buat element cart item
+    var cartItem = document.getElementsByClassName('cart-items')[0];
+    var cartRowContents = `
+                    <div class="cart-item cart-column">
+                        <img class="cart-item-image" src="${image}" width="100" height="100">
+                        <span class="cart-item-title">${title}</span>
+                    </div>
+                    <span class="cart-price cart-column">${price}</span>
+                    <div class="cart-quantity cart-column">
+                        <input class="cart-quantity-input" type="number" value="1">
+                        <button class="btn btn-danger" type="button">REMOVE</button>
+                    </div>`
+    cartRow.innerHTML = cartRowContents;
+    //kalau item sudah dimasukkan
+    var cartItemName = cartItem.getElementsByClassName('cart-item-title');
+    for (var i = 0; i < cartItemName.length; i++) {
+        if (cartItemName[i].innerText == title) {
+        return;
+        }
+    }
+    //masukkan ke cart item
+    cartItem.append(cartRow);
+    //tambah cartRow baru, dan masukkan button ke addEventListener
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCart);
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged);
+    updateTotalCart()
 }
 
 function quantityChanged(e) {
